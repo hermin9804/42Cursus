004cb405dfbbcad354ac1f7941b21e699eb27780
hermin9804 <hermin9804@gmail.com>
2021-11-23 20:48:43 +0900

    modify

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 16:42:23 by mher              #+#    #+#             */
-/*   Updated: 2021/11/21 17:34:05 by mher             ###   ########.fr       */
+/*   Updated: 2021/11/23 20:46:05 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,13 @@ static size_t	ft_get_word_cnt(const char *s, char c)
 	return (cnt);
 }
 
+static const char	*ft_find_to(const char *s, char c)
+{
+	while (*s && *s != c)
+		++s;
+	return (s);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**ret;
@@ -54,7 +61,7 @@ char	**ft_split(char const *s, char c)
 
 	if (!s)
 		return (0);
-	ret = (char **)ft_calloc(sizeof(char *), (ft_get_word_cnt(s, c) + 1));
+	ret = (char **)malloc(sizeof(char *) * (ft_get_word_cnt(s, c) + 1));
 	if (!ret)
 		return (0);
 	i = 0;
@@ -63,8 +70,7 @@ char	**ft_split(char const *s, char c)
 		if (*s != c)
 		{
 			from = (char *)s;
-			while (*s && *s != c)
-				++s;
+			s = ft_find_to(s, c);
 			ret[i] = (char *)malloc(s - from + 1);
 			if (!ret[i])
 				return (ft_malloc_error(ret));
@@ -72,5 +78,6 @@ char	**ft_split(char const *s, char c)
 		}
 		++s;
 	}
+	ret[i] = 0;
 	return (ret);
 }
