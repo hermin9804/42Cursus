557bd7d61a7b85065fc7ca917ab6a06e8eb2611c
hermin9804 <hermin9804@gmail.com>
2022-01-01 17:55:45 +0900

    [modify ] ft_printf print_arg.c"

--- ft_printf/print_arg.c
+++ ft_printf/print_arg.c
@@ -6,7 +6,7 @@
 /*   By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 17:55:12 by mher              #+#    #+#             */
-/*   Updated: 2022/01/01 16:57:11 by mher             ###   ########.fr       */
+/*   Updated: 2022/01/01 17:55:12 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 
 int	print_arg(va_list ap, t_info *info)
 {
-	int		arg_len;
 	char	type;
 
-	arg_len = 0;
 	type = info->type;
 	if (type == 'c')
-		arg_len = print_char(va_arg(ap, int), info);
+		return (print_char(va_arg(ap, int), info));
 	else if (type == 's')
-		arg_len = print_string(va_arg(ap, char *), info);
+		return (print_string(va_arg(ap, char *), info));
 	else if (type == 'd' || type == 'i')
-		arg_len = print_nbr(va_arg(ap, int), info);
+		return (print_nbr(va_arg(ap, int), info));
 	else if (type == 'u' || type == 'x' || type == 'X')
-		arg_len = print_nbr(va_arg(ap, unsigned int), info);
+		return (print_nbr(va_arg(ap, unsigned int), info));
 	else if (type == 'p')
-		arg_len = print_nbr(va_arg(ap, unsigned long), info);
+		return (print_nbr(va_arg(ap, unsigned long), info));
 	else if (type == '%')
-		arg_len = print_char('%', info);
-	return (arg_len);
+		return (print_char('%', info));
+	return (-1);
 }
