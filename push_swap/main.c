aab9b8c64d7b076b5bd05dd056b45e7815032d76
hermin9804 <hermin9804@gmail.com>
2022-04-11 18:15:05 +0900

    push_swap: 인덱스 다시추가, 정렬 방식 변경

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 15:03:48 by mher              #+#    #+#             */
-/*   Updated: 2022/04/11 03:27:30 by mher             ###   ########.fr       */
+/*   Updated: 2022/04/11 18:12:03 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,71 @@ void	print_stack(const t_stack *stack)
 	printf("print-stack, size %d \n", stack->size);
 	while (i < stack->size)
 	{
-		printf("%d", p->data);
+		printf("%d %d", p->data, p->index);
 		printf("\n");
 		p = p->next;
 		i++;
 	}
 }
 
-void	init_info(t_info *info, int *nums)
+void	print_anchor(const t_info *info)
+{
+	int	i;
+
+	i = 0;
+	printf("info->anchor[i].data :");
+	while (i < info->total_size)
+	{
+		printf("%d ", info->anchor[i].data);
+		++i;
+	}
+	printf("\n");
+}
+
+void	init_anchor_data(t_info *info, int *nums)
+{
+	int	i;
+
+	i = 0;
+	info->anchor = (t_anchor *)malloc(info->total_size * sizeof(t_anchor));
+	if (!info->anchor)
+		exit(1);//
+	while (i < info->total_size)
+	{
+		info->anchor[i].data = nums[i];
+		i++;
+	}
+}
+
+
+void	mapping_index(t_info *info)
+{
+	int	i;
+
+	i = 0;
+	while (i < info->total_size)
+	{
+		info->anchor[i].p->index = i;
+		++i;
+	}
+}
+
+void	link_anchor_stack(t_info *info)
+{
+	int	i;
+	t_list	*cul;
+
+	i = 0;
+	cul = info->a.head;
+	while (i < info->total_size)
+	{
+		info->anchor[i].p = cul;
+		cul = cul->next;
+		++i;
+	}
+}
+
+void	init_stack(t_info *info, int *nums)
 {
 	size_t	i;
 	t_list	*tmp;
@@ -37,17 +94,13 @@ void	init_info(t_info *info, int *nums)
 	i = 0;
 	info->a.size = 0;
 	info->b.size = 0;
-	info->max = nums[i];
 	info->a.head = ft_lstnew(nums[i++]);
 	info->a.size++;
 	while (i < info->total_size)
 	{
-		if (nums[i] > info->max)
-			info->max = nums[i];
 		tmp = ft_lstnew(nums[i]);
 		if (!tmp)
 			exit(1);//
-		//ft_lstadd_front(&info->a.head, tmp);
 		ft_lstadd_back(&info->a.head, tmp);
 		info->a.size++;
 		++i;;
@@ -109,6 +162,18 @@ int	*get_nums(char *line, t_info *info)
 	return (nums);
 }
 
+void	init_info(t_info *info, int *nums)
+{
+	size_t	i;
+	t_list	*tmp;
+
+	init_anchor_data(info, nums);
+	init_stack(info, nums);
+	link_anchor_stack(info);
+	quick_sort(info->anchor, 0, info->total_size - 1);
+	mapping_index(info);
+}
+
 int	main(int argc, char **argv)
 {
 	char	*line;
@@ -121,5 +186,6 @@ int	main(int argc, char **argv)
 	nums = get_nums(line, &info);
 	init_info(&info, nums);
 	push_swap(&info);
-	//print_stack(&info.a);
+	print_stack(&info.a);
 }
+
