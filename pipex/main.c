03997e2b5d0b9f4e526f204a90094ba21743f559
hermin9804 <hermin9804@gmail.com>
2022-04-29 19:46:23 +0900

    feat: ipc test

--- pipex/main.c
+++ pipex/main.c
@@ -5,15 +5,65 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/24 18:11:01 by mher              #+#    #+#             */
-/*   Updated: 2022/04/24 18:12:24 by mher             ###   ########.fr       */
+/*   Created: 2022/04/25 23:37:31 by mher              #+#    #+#             */
+/*   Updated: 2022/04/29 19:26:18 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+#include "pipex.h"
 
-int main(int argc, char *argv[])
+static void child(int fd[2])
 {
-	printf("IHIHIHIHI\n");
-	return 0;
+	dup2(fd[FD_OUT], STDOUT_FILENO);
+	char *args[] = {"/bin/echo", "hello", NULL};
+	printf("I'm child!\n");
+	fflush(stdout);
+	execve("/bin/echo", args, NULL);
+	err(EXIT_FAILURE, "exec() failed");
+}
+
+static void parent(int fd[2])
+{
+	char tmp[5];
+	int	cnt;
+
+	printf("I'm parent!\n");
+	cnt = read(fd[FD_IN], tmp, 5);
+	printf("%d\n", cnt);
+	write(STDOUT_FILENO, tmp, 5);
+	exit(EXIT_SUCCESS);
+}
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	pid_t	child_pid;
+	int	fd[2];
+	int	status;
+
+	(void)argv;
+	(void)envp;
+	if (argc < 1)
+		return (0);
+	if (pipe(fd) < 0)
+	{
+		printf("pipe error\n");
+		exit(EXIT_FAILURE);
+	}
+	child_pid = fork();
+	if (child_pid == -1)
+		err(EXIT_FAILURE, "fork() failed");
+	if (child_pid == 0)
+	{
+		child(fd);
+	}
+	else
+	{
+		waitpid(child_pid, &status, 0);
+		parent(fd);
+	}
+	err(EXIT_FAILURE, "shouldn't reach here");
 }
--- pipex/practice/fork_and_exec.c
+++ pipex/practice/fork_and_exec.c
@@ -6,7 +6,7 @@
 /*   By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 23:37:31 by mher              #+#    #+#             */
-/*   Updated: 2022/04/25 23:37:37 by mher             ###   ########.fr       */
+/*   Updated: 2022/04/29 18:15:46 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,26 @@ static void parent(pid_t pid_c)
 
 int	main(void)
 {
-	pid_t ret;
+	pid_t	ret;
+	int	fdA[2];
+	int	fdB[2];
+	
+	if (pipe(fdA) < 0)
+	{
+		printf("pipe error\n");
+		exit(1);
+	}
+	if (pipe(fdB) < 0)
+	{
+		printf("pipe error\n");
+		exit(1);
+	}
 	ret = fork();
 	if (ret == -1)
 		err(EXIT_FAILURE, "fork() failed");
 	if (ret == 0)
-	{
 		child();
-	}
 	else
-	{
 		parent(ret);
-	}
 	err(EXIT_FAILURE, "shouldn't reach here");
 }
