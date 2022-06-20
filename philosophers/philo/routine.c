239531741902151358e94305b3091d62a6c194cf
hermin9804 <hermin9804@gmail.com>
2022-06-20 17:08:31 +0900

    FEAT: routine() 기능 구현

--- philosophers/philo/routine.c
+++ philosophers/philo/routine.c
@@ -6,7 +6,7 @@
 /*   By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 21:13:23 by mher              #+#    #+#             */
-/*   Updated: 2022/06/20 14:50:54 by mher             ###   ########.fr       */
+/*   Updated: 2022/06/20 16:53:54 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	take_forks(t_philo *philo)
 	pthread_mutex_lock(philo->lfork);
 	printf("%d has taken a fork\n", philo->id);
 	pthread_mutex_lock(philo->rfork);
+	printf("%d has taken a fork\n", philo->id);
 }
 
 void	release_forks(t_philo *philo)
@@ -31,9 +32,10 @@ static	int eating(t_philo *philo)
 	pthread_mutex_lock(&(philo->event_lock));
 	usleep(philo->info->tte * 1000);
 	printf("%d is eating\n", philo->id);
+	philo->eat_count++;
 	pthread_mutex_unlock(&(philo->event_lock));
 	release_forks(philo);
-	return (0);
+	return (is_end_simulation(philo));
 }
 
 static int sleeping(t_philo *philo)
@@ -42,13 +44,13 @@ static int sleeping(t_philo *philo)
 	printf("%d is sleeping\n", philo->id);
 	usleep(philo->info->tts * 1000);
 	pthread_mutex_unlock(&(philo->event_lock));
-	return (0);
+	return (is_end_simulation(philo));
 }
 
 static int thinking(t_philo *philo)
 {
 	printf("%d is thinking\n", philo->id);
-	return (0);
+	return (is_end_simulation(philo));
 }
 
 void	*do_routine(void *philo)
