39c45f62634538383b9d445b19e2c792c21840a1
hermin9804 <hermin9804@gmail.com>
2022-06-25 19:17:49 +0900

    REFACTOR: 함수명 변경

--- philosophers/philo/initalizer.c
+++ philosophers/philo/initalizer.c
@@ -6,13 +6,13 @@
 /*   By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 15:36:35 by mher              #+#    #+#             */
-/*   Updated: 2022/06/25 19:13:24 by mher             ###   ########.fr       */
+/*   Updated: 2022/06/25 19:17:34 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	alloc_philo(t_philo **philos, t_info *info)
+int	malloc_philos(t_philo **philos, t_info *info)
 {
 	*philos = (t_philo *)malloc(sizeof(t_philo) * info->nop);
 	if (*philos == NULL)
