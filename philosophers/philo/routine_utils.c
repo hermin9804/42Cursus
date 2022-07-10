fef70156202734b1ba3af14baa8016b4deb3e738
hermin9804 <hermin9804@gmail.com>
2022-07-11 00:21:08 +0900

    REFACTOR: 포크 잡는 순서 제거

--- philosophers/philo/routine_utils.c
+++ philosophers/philo/routine_utils.c
@@ -6,7 +6,7 @@
 /*   By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 16:48:44 by mher              #+#    #+#             */
-/*   Updated: 2022/07/02 22:01:20 by mher             ###   ########.fr       */
+/*   Updated: 2022/07/11 00:20:24 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,14 @@
 
 void	acquire_forks(t_philo *philo)
 {
-	if (philo->id % 2 == 1)
-	{
-		pthread_mutex_lock(philo->lfork);
-		print_log(philo, FORK);
-		pthread_mutex_lock(philo->rfork);
-		print_log(philo, FORK);
-	}
-	else
-	{
-		pthread_mutex_lock(philo->rfork);
-		print_log(philo, FORK);
-		pthread_mutex_lock(philo->lfork);
-		print_log(philo, FORK);
-	}
+	pthread_mutex_lock(philo->lfork);
+	print_log(philo, FORK);
+	pthread_mutex_lock(philo->rfork);
+	print_log(philo, FORK);
 }
 
 void	release_forks(t_philo *philo)
 {
-	if (philo->id % 2 == 1)
-	{
-		pthread_mutex_unlock(philo->lfork);
-		pthread_mutex_unlock(philo->rfork);
-	}
-	else
-	{
-		pthread_mutex_unlock(philo->rfork);
-		pthread_mutex_unlock(philo->lfork);
-	}
+	pthread_mutex_unlock(philo->rfork);
+	pthread_mutex_unlock(philo->lfork);
 }
