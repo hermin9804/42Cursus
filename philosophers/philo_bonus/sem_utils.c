51360f9f136b3764a25f4f8a2c18a7ad253c73c2
hermin9804 <hermin9804@gmail.com>
2022-07-10 23:45:52 +0900

    파일 나누기

--- philosophers/philo_bonus/sem_utils.c
+++ philosophers/philo_bonus/sem_utils.c
@@ -1,30 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   routine_utils.c                                    :+:      :+:    :+:   */
+/*   sem_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/21 16:48:44 by mher              #+#    #+#             */
-/*   Updated: 2022/07/10 23:44:29 by mher             ###   ########.fr       */
+/*   Created: 2022/07/10 23:43:57 by mher              #+#    #+#             */
+/*   Updated: 2022/07/10 23:44:25 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	acquire_forks(t_philo *philo)
+void	set_sem_name(char *sem_name, unsigned int id)
 {
-	if (philo->info->nop != 1)
-		sem_wait(philo->shared->forks_lock);
-	sem_wait(philo->shared->forks);
-	print_log(philo, FORK);
-	sem_wait(philo->shared->forks);
-	print_log(philo, FORK);
-}
+	const char		*suffix = "event_lock";
+	unsigned int	tmp;
+	int				i;
+	int				len;
 
-void	release_forks(t_philo *philo)
-{
-	sem_post(philo->shared->forks);
-	sem_post(philo->shared->forks);
-	sem_post(philo->shared->forks_lock);
+	len = 0;
+	tmp = id;
+	while (tmp != 0)
+	{
+		tmp /= 10;
+		++len;
+	}
+	i = len;
+	while (i)
+	{
+		sem_name[--i] = (id % 10) + '0';
+		id /= 10;
+	}
+	i = 0;
+	while (suffix[i])
+	{
+		sem_name[len + i] = suffix[i];
+		++i;
+	}
+	sem_name[len + i] = '\0';
 }
